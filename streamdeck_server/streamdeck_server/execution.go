2987a33e617e5a3abc0a1c328b2b62ae81702b9f
Brad S <8080871+Brads3290@users.noreply.github.com>
2020-12-28 06:59:03 +1100

    Split the execution code out into three functions depending on the type of the command

--- streamdeck_server/streamdeck_server/execution.go
+++ streamdeck_server/streamdeck_server/execution.go
@@ -1,5 +1,11 @@
 package streamdeck_server
 
+import (
+	"faux-streamdeck/streamdeck_server/config"
+	"github.com/micmonay/keybd_event"
+	"log"
+)
+
 //goland:noinspection GoSnakeCaseUsage
 const (
 	MAX_COMMAND_QUEUE_SIZE = 8
@@ -24,7 +30,47 @@ func runQueueThread(chanQueue chan string) {
 }
 
 func runCommandOnThread(commandGuid string) {
+	var button *config.Button = nil
 
-	// TODO: Implement
+	for i, v := range config.Buttons.Buttons {
+		if v.Id == commandGuid {
+			button = &config.Buttons.Buttons[i]
+			break
+		}
+	}
+
+	if button == nil {
+		log.Println("WARN - Guid has no matching button: ", commandGuid)
+		return
+	}
+
+	for _, v := range button.Commands {
+
+		var err error
+		switch vcasted := v.(type) {
+		case config.ShortcutCommand:
+			err = executeKeyboardShortcut(vcasted)
+		case config.ScriptCommand:
+			err = executeScript(vcasted)
+		case config.ShellCommand:
+			err = executeShellCommand(vcasted)
+		}
+
+		if err != nil {
+			log.Printf("Error executing command \"%s\" for button \"%s\"\n", v.GetCommandType(), button.Name)
+			return
+		}
+	}
+}
+
+func executeKeyboardShortcut(cmd config.ShortcutCommand) error {
+
+}
+
+func executeScript(cmd config.ScriptCommand) error {
+
+}
+
+func executeShellCommand(cmd config.ShellCommand) error {
 
 }
